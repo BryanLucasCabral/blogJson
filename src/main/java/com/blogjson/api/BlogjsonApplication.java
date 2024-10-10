12797bba87836619bc12fde008a111a626eb1802
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-10-10 16:34:58 -0300

    projeto criado

---
+++ src/main/java/com/blogjson/api/BlogjsonApplication.java
@@ -0,0 +1,13 @@
+package com.blogjson.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogjsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogjson/api/BlogjsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogjson.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogjsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
